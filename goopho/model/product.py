dc2b4c08327dbe8739b1d750e3c1785ecdf5f9d8
daveads <the.daveads@gmail.com>
2021-07-05 11:09:04 +0100

    product table

---
+++ goopho/model/product.py
@@ -0,0 +1,18 @@
+from goopho.model import db
+from sqlalchemy.sql import func
+
+class Product(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    small_description = db.Column(db.String(300), nullable=False)
+    
+    long_description = db.Column(db.String(1000), nullable=True)
+    
+    created_on = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    images = db.relationship('Image')
+
+    
