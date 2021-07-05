2f00266522134222e72494bc4eefbe8c2c67b9fd
daveads <the.daveads@gmail.com>
2021-07-05 11:10:45 +0100

    user signup

--- goopho/model/admin.py
+++
@@ -1,7 +0,0 @@
-from goopho.model import db
-
-class Admin(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True) 
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(50))
