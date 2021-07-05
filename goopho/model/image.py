b5692f90d2a8d22eb3f887808a9df2d02a6b5e31
daveads <the.daveads@gmail.com>
2021-07-05 11:08:43 +0100

    image table

---
+++ goopho/model/image.py
@@ -0,0 +1,12 @@
+from goopho.model import db
+from sqlalchemy.sql import func
+
+class Image(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    image = db.Column(db.String(100))
+    
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+
+
