94a76cdc46dd9adbfd471fdd03152e18eec5fed3
daveads <the.daveads@gmail.com>
2021-07-05 11:09:33 +0100

    old password model

---
+++ goopho/model/oldpassword.py
@@ -0,0 +1,11 @@
+from goopho.model import db
+from sqlalchemy.sql import func
+
+
+class Oldpassword():
+    # id
+    # when password was changed
+    # password
+    # user foriegn_key
+    
+    pass
