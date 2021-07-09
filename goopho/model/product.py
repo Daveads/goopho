c42198b7f3c7679cfc4a6bf5d474d0857876982b
daveads <the.daveads@gmail.com>
2021-07-09 22:08:15 +0100

    title & description

--- goopho/model/product.py
+++ goopho/model/product.py
@@ -5,9 +5,9 @@ class Product(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     
-    small_description = db.Column(db.String(300), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
     
-    long_description = db.Column(db.String(1000), nullable=True)
+    description = db.Column(db.String(1000), nullable=True)
     
     created_on = db.Column(db.DateTime(timezone=True), default=func.now())
 
