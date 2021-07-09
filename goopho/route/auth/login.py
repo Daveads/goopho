d0c6b1f705363892a5c18778ed0cad33eb72679d
daveads <the.daveads@gmail.com>
2021-07-09 15:15:21 +0100

    csrf token

--- goopho/route/auth/login.py
+++ goopho/route/auth/login.py
@@ -3,7 +3,7 @@ from goopho.route import User
 from werkzeug.security import check_password_hash
 
 #add flask_jwt
-from flask_jwt_extended import create_access_token, set_access_cookies
+from flask_jwt_extended import create_access_token, set_access_cookies, get_csrf_token
 
 
 logIn = Blueprint('login', __name__)
@@ -31,7 +31,7 @@ def login():
     
     user_data['acct_creation'] = user.acct_create_at
     
-    print(user_data)
+    #print(user_data)
        
     if not user:
         return make_response("could not authenticate", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
@@ -41,10 +41,12 @@ def login():
     if check_password_hash(user.password, auth.password):
         
         access_token = create_access_token(identity=user.public_id)
-
+        csrf_token = get_csrf_token(access_token)
+        
         response = jsonify({"messge": "login successful",
                             'token' : access_token,
-                            'user' : user.username
+                            'user' : user.username,
+                            'csrf_token' : csrf_token 
         })
 	    
         set_access_cookies(response, access_token)
