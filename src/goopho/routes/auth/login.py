f9132326bd62c0e90d3550c9327c7fa10e62bf98
daveads <the.daveads@gmail.com>
2021-07-20 08:07:36 +0100

    rm unncesssary lines of code

--- src/goopho/routes/auth/login.py
+++ src/goopho/routes/auth/login.py
@@ -1,7 +1,6 @@
-import re
-from flask import request, jsonify, make_response, Blueprint
+from flask import request, jsonify, make_response
 
-from flask_restful import Resource, reqparse, abort
+from flask_restful import Resource, abort
 from flasgger import swag_from
 
 
@@ -11,12 +10,6 @@ from werkzeug.security import check_password_hash
 
 #add flask_jwt
 from flask_jwt_extended import create_access_token, set_access_cookies, get_csrf_token
-
-"""
-login_auth = reqparse.RequestParser()
-login_auth.add_argument("username", type=str, help="username required for login", required=True)
-login_auth.add_argument("password", type=str, help="password required for login", required=True)
-"""
 
 class login(Resource):
     
@@ -32,19 +25,7 @@ class login(Resource):
         
         user = User.query.filter_by(username=auth.username).first()
     
-        #just testing out something
-        user_data={}
-    
-        user_data['public_id']= user.public_id
-    
-        user_data['role']= user.roles
-    
-        user_data['email_verif']= user.email_verfication
-    
-        user_data['acct_creation'] = user.acct_create_at
-    
-        #print(user_data)
-       
+      
         if not user:
          return make_response("could not authenticate", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
         
@@ -58,22 +39,13 @@ class login(Resource):
             response = jsonify({
                             'token' : access_token,
                             'csrf_token' : csrf_token,
-                            'email_verfication' : user.email_verfication,
+                            'email_verfication' : user.email_verification,
                             'delete_status' : user.isDeleted
                             })
 	    
             set_access_cookies(response, access_token)
 	
-	  
-            """return jsonify({'token' : access_token,
-        		'message': "login successful"
-                })"""
-     
-	    
             return response
 
  
         return  make_response("could not authenticate", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-
-
-
