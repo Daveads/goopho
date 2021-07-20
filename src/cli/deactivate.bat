6d48fcad2afe325147b36dbea4b739dd4592600a
daveads <the.daveads@gmail.com>
2021-07-20 11:01:42 +0100

    batch script

--- src/cli/deactivate.bat
+++ src/cli/deactivate.bat
@@ -4,5 +4,5 @@ reg delete "HKCU\Environment" /v FLASK_ENV /f
 reg delete "HKCU\Environment" /v FLASK_APP /f
 
 
-REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V FOO
+Rem " REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V FOO "
 
---
+++ src/initiate.bat
@@ -0,0 +1,6 @@
+Rem this activate the scripts
+
+cli/activate.bat
+
+Rem need to understand if-else statments, swicth in batch cmd
+
