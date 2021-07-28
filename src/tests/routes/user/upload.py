e22d3373cea105df417bf2af8e7abe77c728455c
daveads <the.daveads@gmail.com>
2021-07-28 02:06:04 +0100

    upload test

--- src/tests/routes/user/upload.py
+++ src/tests/routes/user/upload.py
@@ -4,36 +4,36 @@ test_url = "http://127.0.0.1:5000/upload"
 
 #token = "dc166ec2-9429-43dd-922c-1c5ce524a172"
 
-#headers = {'X-CSRF-TOKEN': token }
+headers = {
+    'authorization': "gop-tok eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYyNzQzMjMwOCwianRpIjoiZjExMTQ1MzgtMDEzMi00NjRhLThjZTgtZjIyNWUzNmE2YWIwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjEzNjhkNWM5LTg2ODEtNDgxNS05YzgxLTNjZTdlMDFmNjZlZCIsIm5iZiI6MTYyNzQzMjMwOCwiZXhwIjoxNjI4NjQxOTA4fQ.zOX2b-4bXfsGdZaPDdfUHzCi7OSfF1BSUWdsM-Wvw2I"}
+
 
 #headers={'X-CSRF-TOKEN' :  'dc166ec2-9429-43dd-922c-1c5ce524a172'}
 
 
-test_files = {
+multiple_files = [('file', (open('1.jpeg', 'rb'))),
 
-    "file" : (open("0.jpg", "rb"),
+                      ('file', (open('0.jpg', 'rb')))]
 
-              open("1.jpeg", "rb"),
 
-              open("2.jpeg", "rb"),
+#files = {'file': open('0.jpg', 'rb')}
+#"http://127.0.0.1:5000/upload", headers=headers
 
-              open("3.jpg", "rb")),
 
-    "title" : "this is a title",
-
-    "description" : "just testing stuffs"
+myobj = {'title': 'testing',
+        'description' : "just testing"
 }
 
 
+test_response = requests.post(url=test_url, headers=headers, files=multiple_files, data=myobj)
 
 
-test_response = requests.post("http://127.0.0.1:5000/upload", files=test_files)
 
-
-"""
 if test_response.ok:
     print("Upload completed successfully!")
     print(test_response.text)
+    
+    
 else:
     print("Something went wrong!")
-"""
+    print(test_response.json())
