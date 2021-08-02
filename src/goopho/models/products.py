61e6a829e0683a0d100469c808c5dbef35485ab7
daveads <the.daveads@gmail.com>
2021-08-02 12:48:17 +0100

    product public id and better date/time formater for created_on col

--- src/goopho/models/products.py
+++ src/goopho/models/products.py
@@ -1,27 +1,37 @@
 from goopho.models import db
 from sqlalchemy.sql import func
-import datetime
+
+from datetime import datetime 
+
+datenow = datetime.now()
 
 class Product(db.Model):
 
     __tablename__ = "product"
 
     id = db.Column(db.Integer, primary_key=True)
+
+    product_pub_id = db.Column(db.String(50), unique=True, nullable=False)
     
     title = db.Column(db.String(50), nullable=False)
     
     description = db.Column(db.String(1000), nullable=False)
     
-    created_on = db.Column(db.DateTime(timezone=True), nullable=False)
+    created_on = db.Column(db.String(20), nullable=False)
 
     user_pub_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    isAvailable = db.Column(db.Boolean, default=True)
+    
+    isDeleted = db.Column(db.Boolean, default=False)
     
     images = db.relationship('Image', backref='product')
 
     
-    def __init__(self, title, description, user_pub_id):
+    def __init__(self, title, description, user_pub_id, product_pub_id):
 
         self.title = title
         self.description = description
         self.user_pub_id = user_pub_id
-        self.created_on = datetime.datetime.now()
+        self.created_on = datenow.strftime("%d/%m/%Y %H:%M:%S")
+        self.product_pub_id = product_pub_id
