bf0228a9c3b083f160c3112e9cb59c90ba957ee5
daveads <the.daveads@gmail.com>
2021-08-03 10:13:11 +0100

    test routes

--- src/tests/routes/user/upload.py
+++ src/tests/routes/user/upload.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import requests
 
 test_url = "http://127.0.0.1:5000/upload"
@@ -5,15 +7,14 @@ test_url = "http://127.0.0.1:5000/upload"
 #token = "dc166ec2-9429-43dd-922c-1c5ce524a172"
 
 headers = {
-    'authorization': "gop-tok eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYyNzQzMjMwOCwianRpIjoiZjExMTQ1MzgtMDEzMi00NjRhLThjZTgtZjIyNWUzNmE2YWIwIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjEzNjhkNWM5LTg2ODEtNDgxNS05YzgxLTNjZTdlMDFmNjZlZCIsIm5iZiI6MTYyNzQzMjMwOCwiZXhwIjoxNjI4NjQxOTA4fQ.zOX2b-4bXfsGdZaPDdfUHzCi7OSfF1BSUWdsM-Wvw2I"}
-
-
+    'authorization': "gop-tok  eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYyNzk2ODkxNCwianRpIjoiNGUyNGQ3ZWMtMzA2NC00MTQ4LWE2MzAtNWY1OGQ5MmQ2MGE4IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImYwZDVlYjQ4LWFhZDUtNDY2NC1hMmYxLTNmYzM1Mzk2ZDZjZCIsIm5iZiI6MTYyNzk2ODkxNCwiZXhwIjoxNjI5MTc4NTE0fQ.A8xDVV9fJ3yd5zDEnc3lJaQOyUqaUPa1Yuayq-lYFLM"}      
+    
 #headers={'X-CSRF-TOKEN' :  'dc166ec2-9429-43dd-922c-1c5ce524a172'}
 
 
-multiple_files = [('file', (open('1.jpeg', 'rb'))),
+multiple_files = [('file', (open('dave.jpeg', 'rb'))),
 
-                      ('file', (open('0.jpg', 'rb')))]
+                      ('file', (open('dd.jpg', 'rb')))]
 
 
 #files = {'file': open('0.jpg', 'rb')}
